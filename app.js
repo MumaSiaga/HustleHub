85606c327632a41ae93f5ea9b445505dcdf364d7
MualusiLilimu <lilimumualusi37@gmail.com>
2025-08-30 19:25:36 +0200

    saving progress

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express=require('express');
 const path =require('path');
 const app=express();
+
+const service = require('./backend/routes/serviceRoutes');
+
+
 const port=process.env.PORT||3000;
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'public', 'views'));
+app.use('/styles', express.static(path.join(__dirname, 'public','styles')));
+app.use('/scripts', express.static(path.join(__dirname, 'public','scripts')));
 
 
 
-
+app.use('/service',service);
 app.get('/',(req,res)=>{
     res.render('landing');
 });
