d0f0e982aea50c2e2987a342baff7cd8db0ef3b5
MualusiLilimu <lilimumualusi37@gmail.com>
2025-08-30 23:26:41 +0200

    created pages for freelencer

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/Home',function(req,res){
+router.get('/home',function(req,res){
     res.render('serviceHome');
 });
-
+router.get('/profile',function(req,res){
+    res.render('serviceProfile');
+});
+router.get('/learning',function(req,res){
+    res.render('learningHub');
+});
 module.exports = router;
